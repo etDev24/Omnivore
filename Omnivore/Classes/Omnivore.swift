1da6043add6cb8f73e2f0f2bbb0eb87c64ab79a9
mac <syedqamar.a1@gmail.com>
2018-02-28 16:41:58 +0500

    Added bundle support

--- Omnivore/Classes/Omnivore.swift
+++ Omnivore/Classes/Omnivore.swift
@@ -10,8 +10,15 @@ import UIKit
 public class Omnivore: NSObject {
     public init(rootViewController: UIViewController, animated: Bool) {
         super.init()
-        let storyboard = UIStoryboard(name: "Omnivore", bundle: nil)
-        let newViewController = storyboard.instantiateViewController(withIdentifier: "OmnivoreRootVC")
-        rootViewController.present(newViewController, animated: animated, completion: nil)
+        let bundle = Bundle(for: self.classForCoder)
+        if let bundleUrl = bundle.url(forResource: "Omnivore", withExtension: "bundle"){
+            if let bundle = Bundle(url: bundleUrl) {
+                let storyboard = UIStoryboard(name: "Omnivore", bundle: bundle)
+                let newViewController = storyboard.instantiateViewController(withIdentifier: "OmnivoreRootVC")
+                rootViewController.present(newViewController, animated: animated, completion: nil)
+                return
+            }
+        }
+        assertionFailure("Failed to load Bundle")
     }
 }
